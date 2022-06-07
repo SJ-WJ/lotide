8d3ff888f852e5159266fc7f321ceab746d1d555
Siraj <sirajwahaj.ibrahim@gmail.com>
2022-06-07 18:15:16 +0000

    Refactored function.

--- findKey.js
+++ findKey.js
@@ -1,28 +1,22 @@
-// Assertion function
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${[actual]} === ${[expected]}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${[actual]} !== ${[expected]}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
-//Implementation Function
 const findKey = function(object,callback) {
-  for (const key of Object.keys(object)) { //Looping through object keys
+  for (const key of Object.keys(object)) {
 
-    if (callback(object[key])) { //Giving callback value of each key until reaches a truthy value
+    if (callback(object[key])) {
       return key;
     }
   }
 };
 
+module.exports = findKey;
+
 //Test Code
-console.log(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2)); // => "noma"
+// console.log(findKey({
+//   "Blue Hill": { stars: 1 },
+//   "Akaleri":   { stars: 3 },
+//   "noma":      { stars: 2 },
+//   "elBulli":   { stars: 3 },
+//   "Ora":       { stars: 2 },
+//   "Akelarre":  { stars: 3 }
+// }, x => x.stars === 2)); // => "noma"
