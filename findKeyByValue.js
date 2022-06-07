2a1393662c24d54c787a578629d489eba9c32a3e
Siraj <sirajwahaj.ibrahim@gmail.com>
2022-06-07 17:51:39 +0000

    Refactored function.

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,34 +1,25 @@
-//Assertion Function
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${[actual]} === ${[expected]}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${[actual]} !== ${[expected]}`);
-  }
-};
-
-
-//step0. Goal is to take in an object and a value, scan them and return the first key which contains the given value
-//step1. Scan the objects
+const assertEqual = require('./assertEqual');
 
 const findKeyByValue = function(object,value) {
-  for (const key in object) { //Grabbing key
+  for (const key in object) {
 
-    if (object[key] === value) { // Seeing if the object key matches the value
+    if (object[key] === value) {
       return key;
     }
   }
 };
 
-//Test code
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire",
-  kdrama: "Penthouse"
-};
+module.exports = findKeyByValue;
 
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Penthouse"), "kdrama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "comedy");
+//Test code
+// const bestTVShowsByGenre = {
+//   sci_fi: "The Expanse",
+//   comedy: "Brooklyn Nine-Nine",
+//   drama:  "The Wire",
+//   kdrama: "Penthouse"
+// };
+
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "Penthouse"), "kdrama");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "comedy");
