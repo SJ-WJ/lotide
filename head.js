63f65b8d6642a076f6c62f8874834b0f373abf40
Siraj <sirajwahaj.ibrahim@gmail.com>
2022-06-07 01:16:53 +0000

    Seperated the test code and placed into test file.

--- head.js
+++ head.js
@@ -5,10 +5,4 @@ const head = function(array) {
   return array[0];
 };
 
-
-//Test Code
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1,1);
-
-assertEqual(head([5,6,7]), 6);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
