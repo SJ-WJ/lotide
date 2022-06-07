28665c093ebe3f0752fad21de21b49cbf0984fda
Siraj <sirajwahaj.ibrahim@gmail.com>
2022-06-07 01:04:35 +0000

    Refactored function.

--- eqArrays.js
+++ eqArrays.js
@@ -1,23 +1,13 @@
 // Function Implementation
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${[actual]} === ${[expected]}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${[actual]} !== ${[expected]}`);
-  }
-};
-
-
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(array1, array2) {
   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) { // I changed === to !== and it started working, then switch the return statements to make sense
+    if (array1[i] !== array2[i]) {
       return false;
     }
   }
   return true;
 };
 
-//Test Code
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;
