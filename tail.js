ae8f2b351c0e069b0b93f779d1ac30c9c27305fc
Siraj <sirajwahaj.ibrahim@gmail.com>
2022-06-07 01:21:26 +0000

    Seperated the test code and placed into test file.

--- tail.js
+++ tail.js
@@ -6,7 +6,3 @@ const tail = function(array) {
 };
 
 module.exports = tail;
-
-//Test Code
-// const words = tail(["Yo Yo", "Lighthouse", "Labs"]);
-// assertEqual(words.length, 2); // original array should still have 3 elements!
